7cb7cc43dabb408fc2c3a8d65d0c34d33719201e
no95typem <no95typem@gmail.com>
2021-02-18 20:20:39 +0400

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if(n<0) n*=-1;
+    let nString = String(n);
+    let reversedString = "";
+    for(let i = nString.length; i >= 0; i--) reversedString+=nString.charAt(i);
+    return +reversedString;
 }
